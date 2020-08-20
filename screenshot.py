e1434ad2b8a01e48271b69e77ce36afd585b8a15
shashank-naik123 <naikshashank12.sn@gmail.com>
2020-08-20 14:11:29 +0530

    Gui done

--- screenshot.py
+++ screenshot.py
@@ -1,11 +1,31 @@
 import time
 import pyautogui
+import tkinter as tk
 
 def screenshot():
     name=int(round(time.time()*1000))
     name='C:/Users/naiks/Desktop/python projects/screenshot data/{}.png'.format(name)
-    time.sleep(5)
+   
     img= pyautogui.screenshot(name)
     img.show()
 
-screenshot()
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(
+    frame,
+    text="Take Screenshot",
+    command= screenshot
+)
+button.pack(side=tk.LEFT)
+
+
+close = tk.Button(
+    frame,
+    text="QUIT",
+    command= quit
+)
+close.pack()
+
+root.mainloop()
